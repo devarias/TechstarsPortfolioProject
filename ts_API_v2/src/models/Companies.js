d1b43d4eecb282dd12ec247e5738907ff96791e2
devarias <gedafu2005@gmail.com>
2021-03-03 19:32:07 -0500

    Added the relationship between companies and other tables

--- ts_API_v2/src/models/Companies.js
+++ ts_API_v2/src/models/Companies.js
@@ -1,20 +1,40 @@
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
 import { sequelize } from '../database/database';
+import Company_survey from './Company_survey';
 //Data Acces Object design pattern
-const Company = sequelize.define('company', {
-	company_id: {
-		type: Sequelize.UUID,
-		defaultValue: Sequelize.UUIDV4,
-		primaryKey: true
-	},
-	company_name: {
-		type: Sequelize.TEXT
-	},
-	email: {
-		type: Sequelize.TEXT
-	}
-}, {
-	timestamps: false
+const Company = sequelize.define(
+  'company',
+  {
+    company_id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    company_name: {
+      type: Sequelize.TEXT,
+    },
+    email: {
+      type: Sequelize.TEXT,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+Company.hasMany(Company_survey, {
+  foreignKey: 'company_id',
+  sourceKey: 'company_id',
 });
-
+Company.hasMany(Mentor_survey, {
+  foreignKey: 'company_id',
+  sourceKey: 'company_id',
+});
+Company_survey.belongsTo(Company, {
+  foreignKey: 'company_id',
+  sourceKey: 'company_id',
+});
+Company.belongsTo(Mentor_survey, {
+  foreignKey: 'company_id',
+  sourceKey: 'company_id',
+});
 export default Company;
