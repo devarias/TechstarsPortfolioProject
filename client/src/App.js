2d7e5b2130a7f18b1ee9772d0f152b0600e6dfbe
valen2510 <valenjaramillo1025@hotmail.com>
2021-03-04 09:32:09 -0500

    Apply authentication components from Auth0  and routes to implement login components and begin views

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
 //import Calendly from './components/Calendar';
 // import Navbar from './components/Navbar';
-// import {BrowserRouter as Router} from 'react-router-dom';
-import Header from './components/Header'
-import FileUploadPage from './components/uploadFile'
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from './components/Header';
+import FileUploadPage from './components/uploadFile';
+import StartButton from './components/StartButton';
+import SelectionButton from './components/SelectionButton';
+import Spinner from './components/Spinner';
+import './styles/App.css';
 
 function App() {
+
+  const { isLoading, isAuthenticated } = useAuth0();
+  
+
+  if (isLoading) { return (<div className='container'><Spinner/></div>) };
+  if (!isAuthenticated) { return (<div className='container'><StartButton/></div>) };
+
   return ( 
-    <>
-    <div className="header">
-      <Header/>
-    </div>
-    <div>  
-    <FileUploadPage/>
-    </div>
-    </>
+    <Router>
+      <div className="header">
+        <Header/>
+      </div>
+      <div className='container'>
+      <Switch>
+        <Route path="/Home">
+          <FileUploadPage/>
+        </Route>
+        <Route path="/">
+          <SelectionButton name='Matching' path="/Dashboard"/>
+          <SelectionButton name='Scheduling' path="/Home"/>
+        </Route>
+        <Route path="/Dashboard">
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
-import '../styles/header.css';
+import { useLocation } from 'react-router-dom';
 import logoUrl from '../images/logo-dark.png';
+import LogoutButton from './LogoutButton';
+import '../styles/header.css';
 
 function Header() {
+
+  const currentLocation = useLocation();
+
+  const navBar = () => {
+    if (currentLocation.pathname !== '/') {
+      return (<>
+      <a className="active" href="/Home">Home</a>
+      <a href="#schedule">Schedule</a>
+      <a href="#reschedule">Reschedule</a>
+      <a href="#AddMentor">Add mentor</a>
+      <a href="#DelMentor">Delete mentor</a>
+      </>)
+    } 
+  }
+  
     return (
         <div className="logo">
         <a href="https://techstars.com">
         <img src={logoUrl} alt="tecchstarts icon" />
         </a>
         <section className="topnav">
-          <a className="active" href="#home">Home</a>
-          <a href="#schedule">Schedule</a>
-          <a href="#reschedule">Reschedule</a>
-          <a href="#AddMentor">Add mentor</a>
-          <a href="#DelMentor">Delete mentor</a>
+          {navBar()}
+          <LogoutButton/>
         </section>
+        
         </div>
     )
 }
