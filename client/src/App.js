387c39cce003c214a7b892c73a34a8a3f3b42908
valen2510 <valenjaramillo1025@hotmail.com>
2021-03-05 22:27:50 -0500

    First Layout pagfor app

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,57 @@
 import React from 'react';
-//import Calendly from './components/Calendar';
-// import Navbar from './components/Navbar';
-import { useAuth0 } from "@auth0/auth0-react";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Header from './components/Header';
-import FileUploadPage from './components/uploadFile';
 import StartButton from './components/StartButton';
 import SelectionButton from './components/SelectionButton';
 import Spinner from './components/Spinner';
 import './styles/App.css';
+//import Calendly from './components/Calendar';
+// import Navbar from './components/Navbar';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Layout } from 'antd';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import FileUploadPage from './components/uploadFile';
+import NavSider from './components/NavSider';
+
+
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
 
 function App() {
 
   const { isLoading, isAuthenticated } = useAuth0();
-  
 
   if (isLoading) { return (<div className='container'><Spinner/></div>) };
   if (!isAuthenticated) { return (<div className='container'><StartButton/></div>) };
+ 
 
-  return ( 
+  return (
     <Router>
-      <div className="header">
-        <Header/>
-      </div>
-      <div className='container'>
-      <Switch>
-        <Route path="/Home">
-          <FileUploadPage/>
-        </Route>
-        <Route path="/">
-          <SelectionButton name='Matching' path="/Dashboard"/>
-          <SelectionButton name='Scheduling' path="/Home"/>
-        </Route>
-        <Route path="/Dashboard">
-        </Route>
-      </Switch>
-      </div>
-    </Router>
+    <Layout>
+      <Sider >
+        <NavSider/>
+      </Sider>
+         <Layout>
+            <Header >
+            </Header>
+            <Content className="container">
+              <Switch>
+                <Route path="/FileUpload">
+                  <FileUploadPage/>
+                </Route>
+                <Route path="/Dashboard">
+                  Dashboard
+                </Route>
+                <Route path="/">
+                  <SelectionButton name='Matching' path="/Dashboard"/>
+                  <SelectionButton name='Scheduling' path="/Schedule"/>
+                </Route>
+              </Switch>
+            </Content>
+            <Footer ></Footer>
+          </Layout>
+     </Layout>
+     </Router>
   );
 }
 
